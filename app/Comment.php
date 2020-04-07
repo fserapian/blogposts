f33e7015569f0475e7c9c4c30fc826dc4eed8e59
Fadi Serapian <fadi.serapian@gmail.com>
2020-04-07 19:52:28 -0400

    add soft deletes to blog posts and comments tables

--- app/Comment.php
+++ app/Comment.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
+    use SoftDeletes;
+
     // Laravel will look for blog_post_id
     public function blogPost()
     {
